ad09335eded86e49c9a6ed123a046165541fae8f
Basia B <basiab222@gmail.com>
2020-05-25 17:01:55 -0700

    wth

---
+++ build/precache-manifest.9e1b582a60394d9659a24cc64298ee0a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0b9f4a47ed28856fae1dfbd891a86f",
+    "url": "/laurent-landing/index.html"
+  },
+  {
+    "revision": "d389d2fbde16c35bf93f",
+    "url": "/laurent-landing/static/css/main.7190fe51.chunk.css"
+  },
+  {
+    "revision": "d77d87267380f8ba330d",
+    "url": "/laurent-landing/static/js/2.47cc15b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/laurent-landing/static/js/2.47cc15b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d389d2fbde16c35bf93f",
+    "url": "/laurent-landing/static/js/main.f6f28dec.chunk.js"
+  },
+  {
+    "revision": "3ebb9bd5db4673c37386",
+    "url": "/laurent-landing/static/js/runtime-main.52c64cfb.js"
+  },
+  {
+    "revision": "c1b637fd385e0e42c37cbfc96538238e",
+    "url": "/laurent-landing/static/media/amazon.c1b637fd.png"
+  },
+  {
+    "revision": "0d25cc45cdf313ef03b530088c810f14",
+    "url": "/laurent-landing/static/media/applemusic.0d25cc45.svg"
+  },
+  {
+    "revision": "0eea0236e824027727777c637e1c283e",
+    "url": "/laurent-landing/static/media/google.0eea0236.png"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/laurent-landing/static/media/instagram.0e84d592.png"
+  },
+  {
+    "revision": "0f2a6908a974057b9ddbe1c0cbc564a7",
+    "url": "/laurent-landing/static/media/itunes.0f2a6908.svg"
+  },
+  {
+    "revision": "5e53d146a4143d5b64db00662eb9c5e8",
+    "url": "/laurent-landing/static/media/laurent.5e53d146.png"
+  },
+  {
+    "revision": "49e37e37efa98b08c40cd2150c0058ab",
+    "url": "/laurent-landing/static/media/soundcloud.49e37e37.png"
+  },
+  {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/laurent-landing/static/media/spotify.11dc437a.png"
+  },
+  {
+    "revision": "690290e17a0a471070d2044e87fb2d67",
+    "url": "/laurent-landing/static/media/tiktok.690290e1.svg"
+  },
+  {
+    "revision": "7ee556c2de1cafc11447059cfd4fa8a3",
+    "url": "/laurent-landing/static/media/vital.7ee556c2.png"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/laurent-landing/static/media/youtube.07b8560a.png"
+  }
+]);
